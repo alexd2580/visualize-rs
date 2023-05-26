7aba7ebfbd7a75019bc25dfc5be04eb570e04412
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2023-05-26 03:25:25 +0200

    Remove old code

--- src/vulkan/mod.rs
+++ src/vulkan/mod.rs
@@ -394,6 +394,26 @@ impl Vulkan {
             })?)
     }
 
+//
+//     let write_descriptor_sets: Vec<WriteDescriptorSet> = image_infos_vec
+//         .iter()
+//         .zip(descriptor_sets.iter())
+//         .map(|(image_infos, &descriptor_set)| {
+//             let a = WriteDescriptorSet::builder()
+//                 .descriptor_type(DescriptorType::STORAGE_IMAGE)
+//                 .image_info(image_infos)
+//                 .dst_set(descriptor_set)
+//                 .dst_binding(0)
+//                 .dst_array_element(0)
+//                 .build();
+//
+//             return a;
+//         })
+//         .collect();
+//
+//     unsafe { device.update_descriptor_sets(&write_descriptor_sets, &[]) };
+//
+
     unsafe fn render_next_frame(&mut self) -> Result<(), Error> {
         let (present_index, present_image) = self.acquire_next_image()?;
 
